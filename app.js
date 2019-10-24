c5848136b46be11235998bfc3f0240ccd4357c15
merjench <merjen.chary@gmail.com>
2019-10-24 14:10:18 -0400

    geolocation api connected

--- app.js
+++ app.js
@@ -8,5 +8,21 @@ request({ url: url, json: true }, (error, response) => {
     // console.log(response)
     // const data = JSON.parse(response.body)
     //console.log(response.body.currently)
-    console.log(response.body.daily.data[0].summary + 'It is Currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% of rain')
+    if (error) {
+       console.log("Unable to connect to weather server")
+    } else {
+      console.log(response.body.daily.data[0].summary + 'It is Currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% of rain')
+    }
+
+})
+
+const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoibWVyamVuY2giLCJhIjoiY2syNHV1NzJjMGNnYzNucW1sbTExdHFzOSJ9.M9OrpCkpGzbhl9X2vmZzag&limit=1'
+
+request({ url: geocodeUrl, json: true}, (error, response) => {
+      // console.log(response)
+    //
+    const latitude = response.body.features[0].center[1]
+    const longitude = response.body.features[0].center[1]
+    console.log(latitude, longitude)
+
 })
