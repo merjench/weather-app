38f0e8b3e1914ad03f4bb04eee1285116cb7f365
merjench <merjen.chary@gmail.com>
2019-10-24 11:08:57 -0400

    got weather http request

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-console.log('Start')
+const request = require('request')
 
-setTimeout(() => {
-  console.log('2 sec')
-}, 2000)
-
-setTimeout(() => {
-  console.log('o sec')
-}, 0)
-
-
-
-console.log('Done')
+// used darksky.net/dev
+const url = 'https://api.darksky.net/forecast/ab1ac9d64866c213407b85793d328ee2/37.8267,-122.4233'
+// can change info with ?key=value@otherkey=othervalue
+//https://api.darksky.net/forecast/ab1ac9d64866c213407b85793d328ee2/37.8267,-122.4233?unit=si
+request({ url: url, json: true }, (error, response) => {
+    // console.log(response)
+    // const data = JSON.parse(response.body)
+    //console.log(response.body.currently)
+    console.log(response.body.daily.data[0].summary + 'It is Currently ' + response.body.currently.temperature + ' degrees out. There is a ' + response.body.currently.precipProbability + '% of rain')
+})
